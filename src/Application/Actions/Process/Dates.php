15100e93e3464aade5f4951b8c989716c889dc1a
joan.prat.llabres <joan.prat.llabres@upcnet.es>
2021-12-20 00:23:36 +0100

    added selected date

--- src/Application/Actions/Process/Dates.php
+++ src/Application/Actions/Process/Dates.php
@@ -12,37 +12,48 @@
 
             return [
                 '0' => ['date' => '20/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'color' => $randResults[array_rand($randResults,1)],
+                    'selected'=> false,
                 ],
                 '1' => ['date' => '21/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ],
                 '2' => ['date' => '22/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ],
                 '3' => ['date' => '23/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ],
                 '4' => ['date' => '24/12/2021',
                     'color' => 'orange'
+                    ,'selected'=> false,
                 ],
                 '5' => ['date' => '25/12/2021',
                     'color' => 'green'
+                    ,'selected'=> true,
                 ],
                 '6' => ['date' => '26/12/2021',
                     'color' => 'orange'
+                    ,'selected'=> false,
                 ],
                 '7' => ['date' => '27/12/2021',
                     'color' => 'red'
+                    ,'selected'=> false,
                 ],
                 '8' => ['date' => '28/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ],
                 '9' => ['date' => '29/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ],
                 '10' => ['date' => '39/12/2021',
                     'color' => $randResults[array_rand($randResults,1)]
+                    ,'selected'=> false,
                 ]
 
             ];
