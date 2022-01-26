a3e87765984092f48937f7f2e648ac93467e6a4e
joapra <xhlB299T?>
2022-01-26 10:11:43 +0100

    add coro as hcg and type as status

--- src/Application/Actions/Process/Dates.php
+++ src/Application/Actions/Process/Dates.php
@@ -6,57 +6,64 @@
 
     class Dates
     {
+
+
         public function listBaseDates(int $processId): array
         {
             $randResults = array("red", "orange", "green");
+            $arrayColor = array("red"=>"#c40303", "orange"=>"#d48b04", "green"=>"#055605e3");
 
-            return [
+            $items = [
                 '0' => ['date' => '20/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)],
-                    'selected'=> false,
+                    'status' =>$randResults[array_rand($randResults,1)]
+                    ,'selected'=> false
                 ],
                 '1' => ['date' => '21/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '2' => ['date' => '22/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '3' => ['date' => '23/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '4' => ['date' => '24/12/2021',
-                    'color' => 'orange'
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '5' => ['date' => '25/12/2021',
-                    'color' => 'green'
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> true,
                 ],
                 '6' => ['date' => '26/12/2021',
-                    'color' => 'orange'
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '7' => ['date' => '27/12/2021',
-                    'color' => 'red'
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '8' => ['date' => '28/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '9' => ['date' => '29/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ],
                 '10' => ['date' => '39/12/2021',
-                    'color' => $randResults[array_rand($randResults,1)]
+                    'status' =>$randResults[array_rand($randResults,1)]
                     ,'selected'=> false,
                 ]
 
             ];
+            return array_map(function ($item) use ($arrayColor){
+                $item['color'] = $arrayColor[$item['status']];
+                return $item;
+            },$items);
 
         }
     }
